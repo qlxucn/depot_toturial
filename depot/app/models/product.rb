5278ec51a536da088f223e7d4d859a32715e2edd
qlxucn <qlxucn@gmail.com>
2013-01-12 00:52:18 -0800

    Iteration C1: Add controller store

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+
   attr_accessible :description, :image_url, :price, :title
 
   validates :description, :image_url, :title, :presence => true
